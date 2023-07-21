8462002ac7ab7f575371f262b68327341d9358ee
ebitech02 <ebipresh02@gmail.com>
2023-07-21 17:05:10 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        'widescreen': {'raw':'(min-aspect-ratio: 3/2)'},
+        'tailscreen': {'raw': '(min-aspect-ratio: 13/20)'},
+      }
+    },
+  },
+  plugins: [],
+}
